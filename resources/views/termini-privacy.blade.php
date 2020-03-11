76cede23cc716d2c1ef748c07efcf471922169bb
fedPass <federica.passanante@gmail.com>
2020-03-11 17:47:14 +0100

    fixed margin top pag termini

--- resources/views/termini-privacy.blade.php
+++ resources/views/termini-privacy.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.public')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <div class="container mt-5">
+        <div class="row mt-3">
           <div class="col-4 mt-5 mb-5">
             <div class="list-group" id="list-tab" role="tablist">
 
@@ -16,17 +16,19 @@
           </div>
           <div class="col-8 mt-5 mb-5">
             <div class="tab-content" id="nav-tabContent">
-              <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list"><p>Termini del Servizio per Utenti europei
-              Leggi attentamente i presenti Termini del Servizio per Utenti europei (“Termini”) in quanto forniscono informazioni importanti riguardanti diritti, rimedi giuridici e obblighi pertinenti all’utente. Accedendo o utilizzando la Piattaforma Airbnb, accetti di rispettare e di essere vincolato dai presenti Termini.</p></div>
-              <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list"><p>Politiche di non discriminazione
-              Il nostro impegno a favore dell'inclusione e del rispetto tra membri
+              <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+                  <h2 class="text-primary">Termini del Servizio per Utenti europei</h2>
+                     <p> Leggi attentamente i presenti Termini del Servizio per Utenti europei (“Termini”) in quanto forniscono informazioni importanti riguardanti diritti, rimedi giuridici e obblighi pertinenti all’utente. Accedendo o utilizzando la Piattaforma Airbnb, accetti di rispettare e di essere vincolato dai presenti Termini.</p>
+          </div>
+              <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+                  <h2 class="text-primary">Politiche di non discriminazione</h2>
+                  <p>Il nostro impegno a favore dell'inclusione e del rispetto tra membri
               L'essenza di Airbnb è quella di essere una community aperta che facilita gli scambi internazionali e rende possibili esperienze importanti e condivise tra persone di tutto il mondo. Di tale community fanno parte milioni di utenti, provenienti in pratica da ogni Paese del globo. La composizione della community è estremamente varia e i suoi membri hanno culture, valori e regole diversissimi tra loro.</p></div>
-              <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list"><p>Termini del Servizio di Pagamento
-              Leggi attentamente i presenti Termini del Servizio di Pagamento (“Termini di Pagamento”) in quanto forniscono informazioni importanti riguardanti diritti, rimedi e obblighi dell’utente. Utilizzando i Servizi di Pagamento (come definiti di seguito), l’utente accetta di rispettare e di essere vincolato dai presenti Termini di Pagamento.
+              <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
+                  <h2 class="text-primary">Termini del Servizio di Pagamento</h2>
+                  <p>Leggi attentamente i presenti Termini del Servizio di Pagamento (“Termini di Pagamento”) in quanto forniscono informazioni importanti riguardanti diritti, rimedi e obblighi dell’utente. Utilizzando i Servizi di Pagamento (come definiti di seguito), l’utente accetta di rispettare e di essere vincolato dai presenti Termini di Pagamento.
               La Sezione 22 dei presenti Termini del Servizio di Pagamento contiene una clausola sull’arbitrato e sulla rinuncia a class action o azioni collettive che si applica a tutti i Membri di Airbnb. Se il paese di residenza dell’utente si trova negli Stati Uniti, tale disposizione si applica a tutte le controversie con Airbnb Payments. Se il paese di residenza dell’utente si trova al di fuori degli Stati Uniti, tale disposizione si applica a qualsiasi azione intentata contro Airbnb Payments negli Stati Uniti. La suddetta clausola si applica alla risoluzione delle controversie con Airbnb Payments. Accettando i presenti Termini di Pagamento, l’utente accetta di essere vincolato a tale clausola sull’arbitrato e sulla rinuncia ad azioni collettive. Si prega di leggerla attentamente.
               Se il paese di residenza dell’utente si trova nello Spazio economico europeo (“SEE”), l’utente può accedere alla piattaforma di risoluzione online delle controversie della Commissione europea qui: https://ec.europa.eu/consumers/odr. Si prega di notare che Airbnb Payments non è impegnata o tenuta a utilizzare un organismo per la risoluzione delle controversie alternativo per risolvere le controversie coi consumatori.</p></div>
-              <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list"><p>Grazie per aver scelto di utilizzare Airbnb! La tua fiducia è importante per noi e ci impegniamo a proteggere la privacy e la sicurezza dei tuoi dati personali i. Le informazioni che vengono condivise con noi ci aiutano a fornire un’esperienza di qualità con Airbnb. Abbiamo un teamdedicato alla privacy che è impegnato a proteggere tutti i dati personali che raccogliamo e assicurare che i dati personali siano gestiti correttamente in tutto il mondo.</p></div>
-            </div>
           </div>
         </div>
         {{-- <div class="row">
