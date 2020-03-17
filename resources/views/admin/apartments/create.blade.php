3e95161cd63324bf81c30f55a227e641f662a724
fedPass <federica.passanante@gmail.com>
2020-03-17 11:34:12 +0100

    corretto form create

--- resources/views/admin/apartments/create.blade.php
+++ resources/views/admin/apartments/create.blade.php
@@ -226,8 +226,8 @@
                     <hr>
                     <div class="row form-group d-flex justify-content-center">
                         <div class="custom-control custom-switch">
-                          <input type="checkbox" class="custom-control-input input-visibilita" id="visibilita-{{$apartment->id}}" data-id="{{$apartment->id}}">
-                          <label class="custom-control-label" for="visibilita-{{$apartment->id}}">Visibilità annuncio</label>
+                          <input type="checkbox" class="custom-control-input input-visibilita" id="visibilita">
+                          <label class="custom-control-label" for="visibilita">Visibilità annuncio</label>
                         </div>
                     </div>
                     <div class="row">
