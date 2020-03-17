6b20796fb4d7469f275b04d624ebdea8c5f1c771
lucapu88 <caputoluca88@gmail.com>
2020-03-17 10:46:52 +0100

    modifica card index public

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -5,17 +5,16 @@
   <div class="row">
     <div class="results-container">
       @forelse ($apartments as $apartment)
-      <div class="col-6 col-md-4 col-lg-3 mb-3">
-        <div class="card">
-          <a href="{{route('apartments.show', $apartment->id)}}" class="btn btn-primary">
+      <div class="col-sm-12 col-md-3 col-lg-3">
+        <a href="{{route('apartments.show', $apartment->id)}}" class="card-click text-decoration-none">
+        <div class="card btn btn-primary card-results">
             <img class="img-thumbnail" src="{{ $apartment->img }}" alt="Immagine appartamento">
             <div class="card-body">
               <h5 class="card-title">{{ $apartment->titolo }}</h5>
-              <p class="card-text">{{ $apartment->descrizione }}</p>
-              {{-- <a href="{{route('apartments.show', $apartment->id)}}" class="btn btn-primary">Visualizza dettagli</a> --}}
+              {{-- <p class="card-text">{{ $apartment->descrizione }}</p> --}}
             </div>
-          </a>
         </div>
+        </a>
       </div>
       @empty
       <p>Non ci sono ancora appartamenti da mostrare</p>
