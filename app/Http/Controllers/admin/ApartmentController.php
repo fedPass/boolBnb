00adc713633d5f23cfb61dc5205ddbdb2cf9b394
ermannod <56300803+ermannod@users.noreply.github.com>
2020-03-24 15:53:13 +0100

    Dropzone

--- app/Http/Controllers/admin/ApartmentController.php
+++ app/Http/Controllers/admin/ApartmentController.php
@@ -285,4 +285,37 @@ class ApartmentController extends Controller
        //return  response()->json($status);
 
     }
+
+
+    // public function uploadimg(Request $request)
+    // {
+    //   if($request->hasFile('file')) {
+    //
+    //      // Upload path
+    //      $destinationPath = 'files/';
+    //
+    //      // Create directory if not exists
+    //      if (!file_exists($destinationPath)) {
+    //         mkdir($destinationPath, 0755, true);
+    //      }
+    //
+    //      // Get file extension
+    //      $extension = $request->file('file')->getClientOriginalExtension();
+    //
+    //      // Valid extensions
+    //      $validextensions = array("jpeg","jpg","png","pdf");
+    //
+    //      // Check extension
+    //      if(in_array(strtolower($extension), $validextensions)){
+    //
+    //        // Rename file
+    //        $fileName = str_slug(Carbon::now()->toDayDateTimeString()).rand(11111, 99999) .'.' . $extension;
+    //
+    //        // Uploading file to given path
+    //        $request->file('file')->move($destinationPath, $fileName);
+    //
+    //      }
+    //
+    //    }
+    // }
 }
