a1f40c228937718633b79305768b42b036f33c4f
fedPass <federica.passanante@gmail.com>
2020-03-25 21:13:18 +0100

    sistemata visualizzazione cards in base a promozione, promozione scaduta o mai attivata

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -70,69 +70,78 @@
 
         <div class="row no-promo-section mt-3 mb-3">
             @forelse ($apartments as $apartment)
-                @if (($apartment->sponsors)->isEmpty())
-                    <div class="col-12 col-md-6 col-lg-4 mb-3">
-                        <div class="card">
-                            <div class="card-img">
-                                @if ($apartment->visibilita == 1)
-                                    <img class="img-thumbnail" src="{{asset('storage/' . $apartment->img)}}" alt="Immagine appartamento">
-                                @else
-                                    <img class="img-thumbnail apt-not-visible" src="{{asset('storage/' . $apartment->img)}}" alt="Immagine appartamento">
-                                @endif
-                            </div>
-                          <div class="card-body">
-                              <a class="text-decoration-none" href="{{ route('admin.apartments.show', ['apartment' => $apartment->id]) }}">
-                                  @if ($apartment->visibilita == 1)
-                                      <h5 class="card-title customJS">{{ $apartment->titolo }}</h5>
-                                  @else
-                                      <h5 class="card-title text-dark">{{ $apartment->titolo }}</h5>
-                                  @endif
-
-                              </a>
-                            <div class="row d-lg-flex align-items-lg-center">
-                                <div class="col-12 col-xl-4 d-flex justify-content-center btn-apartment-crud">
-                                    <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}" class="btn btn-primary float-left">Modifica</a>
-                                </div>
-                                <div class="col-12 col-xl-4 d-flex justify-content-center btn-apartment-crud">
-                                    <a id="stat-btn" href="{{ route('admin.apartments.show', ['apartment' => $apartment->id]) }}#graphic" class="btn btn-primary float-right">Statistiche</a>
-                                </div>
-                                <div class="col-12 col-xl-4 d-flex justify-content-center btn-apartment-crud">
-                                    <form action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->id])}}" method="post" onclick="return confirm('Sei sicuro di voler eliminare questo appartamento?')">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="btn btn-outline-danger">Elimina</button>
-                                    </form>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12 btn-apartment-crud">
+                @foreach ($apartment->sponsors as $time)
+                    @php
+                        $expired_date = $time->pivot->due_date;
+                        // // $current_date = Carbon::now();
+                        // // $diff_in_hours = $to->diffInHours($from);
+                        $diff_in_hours = now()->diffInHours($expired_date);
+                    @endphp
+                @endforeach
+                    {{-- @if (($apartment->sponsors)->isEmpty()) --}}
+                    @if ((($apartment->sponsors)->isEmpty()) || (($apartment->sponsors)->isNotEmpty() && now() > $expired_date))
+                        <div class="col-12 col-md-6 col-lg-4 mb-3">
+                            <div class="card">
+                                <div class="card-img">
                                     @if ($apartment->visibilita == 1)
-                                        <a href="{{ route('admin.apartments.promo', $apartment->id)}}" class="btn btn-primary" id="promo-btn">Promuovi questo appartamento</a>
+                                        <img class="img-thumbnail" src="{{asset('storage/' . $apartment->img)}}" alt="Immagine appartamento">
                                     @else
-                                        <a href="{{ route('admin.apartments.promo', $apartment->id)}}" class="btn btn-primary disabled" id="promo-btn">Promuovi questo appartamento</a>
+                                        <img class="img-thumbnail apt-not-visible" src="{{asset('storage/' . $apartment->img)}}" alt="Immagine appartamento">
                                     @endif
-
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-12 mt-3 d-flex justify-content-center">
-                                    <div class="custom-control custom-switch">
-        {{--                              <input type="checkbox" class="custom-control-input input-visibilita" id="visibilita-{{$apartment->id}}" data-id="{{$apartment->id}}" {{($apartment->visibilita == "1") ? 'checked' : ""}}>--}}
-        {{--                              <label class="custom-control-label" for="visibilita-{{$apartment->id}}">Visibilità annuncio</label>--}}
-                                        <input type="checkbox" data-id="{{ $apartment->id }}" name="visibilita" class="js-switch" {{ $apartment->visibilita == 1 ? 'checked' : '' }}>
+                              <div class="card-body">
+                                  <a class="text-decoration-none" href="{{ route('admin.apartments.show', ['apartment' => $apartment->id]) }}">
+                                      @if ($apartment->visibilita == 1)
+                                          <h5 class="card-title customJS">{{ $apartment->titolo }}</h5>
+                                      @else
+                                          <h5 class="card-title text-dark">{{ $apartment->titolo }}</h5>
+                                      @endif
+
+                                  </a>
+                                <div class="row d-lg-flex align-items-lg-center">
+                                    <div class="col-12 col-xl-4 d-flex justify-content-center btn-apartment-crud">
+                                        <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}" class="btn btn-primary float-left">Modifica</a>
+                                    </div>
+                                    <div class="col-12 col-xl-4 d-flex justify-content-center btn-apartment-crud">
+                                        <a id="stat-btn" href="{{ route('admin.apartments.show', ['apartment' => $apartment->id]) }}#graphic" class="btn btn-primary float-right">Statistiche</a>
+                                    </div>
+                                    <div class="col-12 col-xl-4 d-flex justify-content-center btn-apartment-crud">
+                                        <form action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->id])}}" method="post" onclick="return confirm('Sei sicuro di voler eliminare questo appartamento?')">
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-outline-danger">Elimina</button>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 btn-apartment-crud">
                                         @if ($apartment->visibilita == 1)
-                                            <label class="js-switch" for="visibilita-{{$apartment->id}}">Visibile</label>
+                                            <a href="{{ route('admin.apartments.promo', $apartment->id)}}" class="btn btn-primary" id="promo-btn">Promuovi questo appartamento</a>
                                         @else
-                                            <label class="js-switch" for="visibilita-{{$apartment->id}}">Non visibile</label>
+                                            <a href="{{ route('admin.apartments.promo', $apartment->id)}}" class="btn btn-primary disabled" id="promo-btn">Promuovi questo appartamento</a>
                                         @endif
 
                                     </div>
                                 </div>
+                                <div class="row">
+                                    <div class="col-12 mt-3 d-flex justify-content-center">
+                                        <div class="custom-control custom-switch">
+            {{--                              <input type="checkbox" class="custom-control-input input-visibilita" id="visibilita-{{$apartment->id}}" data-id="{{$apartment->id}}" {{($apartment->visibilita == "1") ? 'checked' : ""}}>--}}
+            {{--                              <label class="custom-control-label" for="visibilita-{{$apartment->id}}">Visibilità annuncio</label>--}}
+                                            <input type="checkbox" data-id="{{ $apartment->id }}" name="visibilita" class="js-switch" {{ $apartment->visibilita == 1 ? 'checked' : '' }}>
+                                            @if ($apartment->visibilita == 1)
+                                                <label class="js-switch" for="visibilita-{{$apartment->id}}">Visibile</label>
+                                            @else
+                                                <label class="js-switch" for="visibilita-{{$apartment->id}}">Non visibile</label>
+                                            @endif
+
+                                        </div>
+                                    </div>
+                                </div>
+                              </div>
                             </div>
-                          </div>
                         </div>
-                    </div>
-                @endif
+                    @endif
             @empty
                 <div class="col-12 mt-5 mb-5">
                     <div class="d-flex align-items-center flex-column text-center" >
