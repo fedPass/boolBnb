9d1a9ed81fe408cc11ae054a6d53a30ff70dccd8
lucapu88 <caputoluca88@gmail.com>
2020-03-26 12:39:44 +0100

    modifica apartment index lato public: scroll appartamenti in promo

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -45,39 +45,40 @@
     </div>
       {{--      end filter area     --}}
     <div class="results-container col-10">
-      <div class="row justify-content-center evidence-container evidence">
+      <div class="row justify-content-center evidence-container">
           <h1>Appartamenti in promozione</h1>
-        <div class="col-sm-12 in-evidenza mx-auto">
+        <div class="col-sm-12 mx-auto evidence">
           @foreach ($apartmentsAll as $apartment)
-              @if (($apartment->sponsors)->isNotEmpty())
-                @foreach ($apartment->sponsors as $time)
-                    @php
-                        $expired_date = $time->pivot->due_date;
-                        // // $current_date = Carbon::now();
-                        // // $diff_in_hours = $to->diffInHours($from);
-                        $diff_in_hours = now()->diffInHours($expired_date);
-                    @endphp
-                    @if (now() <= $expired_date)
+            @if (($apartment->sponsors)->isNotEmpty())
+              @foreach ($apartment->sponsors as $time)
+                @php
+                    $expired_date = $time->pivot->due_date;
+                    // // $current_date = Carbon::now();
+                    // // $diff_in_hours = $to->diffInHours($from);
+                    $diff_in_hours = now()->diffInHours($expired_date);
+                @endphp
+                @if (now() <= $expired_date)
             <div class="col-lg-3 col-md-6 text-center">
               <a href="{{route('apartments.show', $apartment->id)}}" class="card-click text-decoration-none">
               @if ($apartment->visibilita == 1)
                   <img class="custom-img" src="{{asset('storage/' . $apartment->img)}}" alt="Immagine appartamento">
-                  <h5 class="text-white">{{ $apartment->titolo }}</h5>
+                  <h5 class="text-white promo-title">{{ $apartment->titolo }}</h5>
               @endif
               </a>
             </div>
-          @endif
-      @endforeach
+              @endif
+            @endforeach
           @endif
         @endforeach
         </div>
       </div>
+      <div class="results-title col-12">
+        <h1>Risultati di ricerca</h1>
+      </div>
       <section class="container" id="resultApartmentSection">
-        <div class="results-title col-12">
-          <h1>Risultati di ricerca</h1>
-        </div>
-          @forelse ($apartments as $apartment)
-            @foreach ($apartment->sponsors as $time)
+
+         @forelse ($apartments as $apartment)
+          {{--   @foreach ($apartment->sponsors as $time)
                 @php
                     $expired_date = $time->pivot->due_date;
                     // // $current_date = Carbon::now();
@@ -85,8 +86,8 @@
                     $diff_in_hours = now()->diffInHours($expired_date);
                 @endphp
             @endforeach
-                {{-- @if (($apartment->sponsors)->isEmpty()) --}}
-                @if ((($apartment->sponsors)->isEmpty()) || (($apartment->sponsors)->isNotEmpty() && now() > $expired_date))
+                {{-- @if (($apartment->sponsors)->isEmpty())
+                @if ((($apartment->sponsors)->isEmpty()) || (($apartment->sponsors)->isNotEmpty() && now() > $expired_date)) --}}
         <div class="col-12 col-sm-9 col-md-5 col-lg-4">
           <a href="{{route('apartments.show', $apartment->id)}}" class="card-click text-decoration-none">
           <div class="btn btn-primary card-results">
@@ -101,7 +102,7 @@
           </div>
           </a>
         </div>
-      @endif
+      {{-- @endif --}}
           @empty
           <p class="text-center">Non ci sono ancora appartamenti da mostrare</p>
           @endforelse
