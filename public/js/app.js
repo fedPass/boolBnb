d99233d3d72aab80fdf34d5795dac644e69036e9
lucapu88 <caputoluca88@gmail.com>
2020-03-27 15:40:33 +0100

    modifica visualizzazione immagini in show

--- public/js/app.js
+++ public/js/app.js
@@ -49588,6 +49588,16 @@ $(document).ready(function () {
     $('.filters-container').slideUp(); //la navbar scompare
   }); // }
   // -----FILTERS END-----------//
+  //--------CAROUSEL------------//
+  // $(".leftArrow").click(function () { //al click sul pulsante di sinistra
+  //   var leftPos = $('.previev').scrollLeft();
+  //   $(".previev").animate({scrollLeft: leftPos - 500}, 800); //sottraggo 500px alle foto
+  // });
+  // $(".rightArrow").click(function () { //al click sul pulsante di destra
+  //   var leftPos = $('.previev').scrollLeft();
+  //   $(".previev").animate({scrollLeft: leftPos + 500}, 800); //aggiungo 500px alle foto
+  // });
+  //--------CAROUSEL END------------//
   // -----MESSAGGI-----------//
 
   $('.message-recev').on("click", function () {
@@ -49982,8 +49992,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\boolean\boolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\boolean\boolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\lucar\Desktop\Boolean #8\boolBnb.git\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\lucar\Desktop\Boolean #8\boolBnb.git\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
