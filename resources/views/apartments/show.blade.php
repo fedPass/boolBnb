ec3e504851c6c18acf20cc2c4d31d857d023cb26
lucapu88 <caputoluca88@gmail.com>
2020-03-28 15:36:15 +0100

    traduzioni della index-public e show-public

--- resources/views/apartments/show.blade.php
+++ resources/views/apartments/show.blade.php
@@ -5,23 +5,23 @@
     <div class="ul-left col-lg-6">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link text-white" href="#title">Panoramica</a>
+          <a class="nav-link text-white" href="#title">{{__('show-public.navLinkOverv')}}</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#services">Servizi</a>
+          <a class="nav-link text-white" href="#services">{{__('show-public.navLinkServ')}}</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link text-white" href="#maps">La posizione</a>
+            <a class="nav-link text-white" href="#maps">{{__('show-public.navLinkPos')}}</a>
         </li>
       </ul>
     </div>
     <div class="ul-right col-lg-6">
       <ul class="navbar-nav float-right">
         <li class="nav-item">
-          <a class="nav-link text-white" href="#"><i class="far fa-share-square"></i> Condividi</a>
+          <a class="nav-link text-white" href="#"><i class="far fa-share-square"></i> {{__('show-public.navLinkShare')}}</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#"><i class="far fa-heart"></i> Salva</a>
+          <a class="nav-link text-white" href="#"><i class="far fa-heart"></i> {{__('show-public.navLinkSave')}}</a>
         </li>
       </ul>
     </div>
@@ -56,40 +56,40 @@
     </div> --}}
   </div>
 </div>
-<div class="container">
+<div class="container" id="title">
   <div class="row">
     <div class="col-lg-7 col-sm-12 scrol-left">
-      <div class="title" id="title">
+      <div class="title">
         <h2>{{$apartment->titolo}}</h2>
         <div class="user-container float-right">
-          <strong>Proprietario</strong>
+          <strong>{{__('show-public.infoOwn')}}</strong>
           <p class="text-center">{{$apartment->user->first_name}}</p>
         </div>
       </div>
       <div class="info section">
         <a href="#">{{$apartment->cita}}</a>
-        <p>Stanze: {{$apartment->stanze}},  Posti letto: {{$apartment->posti_letto}}, Bagni: {{$apartment->bagni}}</p>
+        <p>{{__('show-public.infoRooms')}}: {{$apartment->stanze}},  {{__('show-public.infoBed')}}: {{$apartment->posti_letto}}, {{__('show-public.infoBat')}}: {{$apartment->bagni}}</p>
       </div>
       <div class="other-info-container section">
         <div class="other-info">
-          <strong><i class="fas fa-home"></i> Casa intera</strong>
-          <p>Appartamento: sarà a tua completa disposizione.</p>
+          <strong><i class="fas fa-home"></i> {{__('show-public.infoHouse')}}</strong>
+          <p>{{__('show-public.infoApart')}}</p>
         </div>
         <div class="other-info">
-          <strong><i class="fas fa-broom"></i> Pulizia perfetta</strong>
-          <p>4 ospiti recenti hanno affermato che questo alloggio ha una pulizia impeccabile.</p>
+          <strong><i class="fas fa-broom"></i> {{__('show-public.infoClean')}}</strong>
+          <p>{{__('show-public.infoOsp')}}</p>
         </div>
         <div class="other-info">
-          <strong><i class="fas fa-map-marker-alt"></i> Ottima posizione</strong>
-          <p>Il 100% degli ospiti recenti ha valutato la posizione con 5 stelle.</p>
+          <strong><i class="fas fa-map-marker-alt"></i> {{__('show-public.infoPos')}}</strong>
+          <p>{{__('show-public.info100')}}</p>
         </div>
       </div>
-      <div class="description section">
-        <h3>Descrizione</h3>
+      <div class="description section" id="services">
+        <h3>{{__('show-public.Desc')}}</h3>
         <p>{{$apartment->descrizione}}</p>
       </div>
-      <div class="services-container section" id="services">
-        <strong>Servizi</strong>
+      <div class="services-container section">
+        <strong>{{__('show-public.Serv')}}</strong>
         <div class="services">
           <div class="col-lg-6">
             @forelse ($apartment->options as $option)
@@ -102,22 +102,22 @@
       </div>
       <div class="other-info-container section">
         <div class="other-info">
-          <h2>Ospitato da {{$apartment->user->first_name}}</h2>
+          <h2>{{__('show-public.Hosted')}} {{$apartment->user->first_name}}</h2>
         </div>
         <div class="other-info">
-          Hello my name is {{$apartment->user->first_name}}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          {{__('show-public.hello')}} {{$apartment->user->first_name}}. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </div>
       </div>
     </div>
     <div class="col-lg-5 fix-right">
       <div class="card-scroll" >
         <div class="card-body">
-          <h3 class="card-title">Scrivi al proprietario</h5>
-          <small>* Campi obbligatori</small>
+          <h3 class="card-title">{{__('show-public.Write')}}</h5>
+          <small>* = {{__('show-public.Required')}}</small>
           <form action="{{--inserire la rotta che ti porta alla view grazie--}}" method="post">
             @csrf
             <div class="form-group">
-              <label for="name">Nome*</label>
+              <label for="name">{{__('show-public.name')}}*</label>
               <input type="text" class="form-control" name='nome' id="name"  placeholder="Nome" required>
             </div>
             <div class="form-group">
@@ -125,15 +125,15 @@
               <input type="email" class="form-control" name='email_mittente' id="email" placeholder="{{ Auth::user() ? Auth::user()->email : 'email' }}" value="{{ Auth::user() ? Auth::user()->email : '' }}" required>
             </div>
             <div class="form-group">
-              <label for="subject">Oggetto*</label>
+              <label for="subject">{{__('show-public.obj')}}*</label>
               <input type="text" class="form-control" name='oggetto' id="subject" placeholder="Oggetto" required>
             </div>
             <div class="form-group">
-              <label for="message">Messaggio*</label>
+              <label for="message">{{__('show-public.mess')}}*</label>
               <textarea class="form-control" id="message" placeholder="Inserisci qui il tuo messaggio..." name="messaggio" rows="9" required></textarea>
               <input type="hidden" name="apartment_id" value="{{$apartment->id}}">
             </div>
-            <button type="submit" class="btn btn-primary">Invia</button>
+            <button type="submit" class="btn btn-primary">{{__('show-public.send')}}</button>
             @if(session()->has('message'))
             <div class="alert alert-danger disappear" >
                 <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
