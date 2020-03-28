e0055756c764f0b8b7d08cbd9a3c359f0b2a0604
giovdigua <giovdigua@gmail.com>
2020-03-28 08:08:20 +0100

    fix minor visual problem in aortment.index

--- resources/views/layouts/partials/pagination_data.blade.php
+++ resources/views/layouts/partials/pagination_data.blade.php
@@ -6,13 +6,9 @@
                 <div class="btn btn-primary card-results">
                     <div class="card-body">
                         @if (($apartment->images)->isNotEmpty())
-                            {{--                      @php--}}
-                            {{--                          $copertina = $apartment->images->first()->filename--}}
-                            {{--                          // $copertina = $apartment->images()->first()--}}
-                            {{--                      @endphp--}}
-                            <img class="custom-img" src="{{asset('uploads/images/'. $apartment->id . '/' . $apartment->image->first()->filename)}}" alt="Immagine appartamento . {{$apartment->titolo}}">
+                            <img class="custom-img" src="{{asset('uploads/images/'. $apartment->id . '/' . $apartment->images->first()->filename)}}" alt="Immagine appartamento . {{$apartment->titolo}}">
                         @else
-                            <img class="custom-img" src="" alt="Immagine appartamento . {{$apartment->titolo}}">
+                            <img class="custom-img" src="https://images.pexels.com/photos/186077/pexels-photo-186077.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Immagine appartamento . {{$apartment->titolo}}">
                         @endif
                         <h5 class="text-white promo-title">{{ $apartment->titolo }}</h5>
                     <div class="card-text">
@@ -20,6 +16,7 @@
                         <small>Stanze: {{$apartment->stanze}},  Posti letto: {{$apartment->posti_letto}}, Bagni: {{$apartment->bagni}}</small>
                         <p class="small-text smallJS">{{$apartment->indirizzo}}</p>
                     </div>
+                </div>
                 </div>
             </a>
         </div>
